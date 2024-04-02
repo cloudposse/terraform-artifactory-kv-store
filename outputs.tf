a4d32dc40b7b963911f071ac52b5a3dfaea58688
korenyoni <10080107+korenyoni@users.noreply.github.com>
2024-04-02 00:28:45 -0400

    Update README and add tests.

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "id" {
-  description = "ID of the created example"
-  value       = module.this.enabled ? module.this.id : null
-}
-
 output "values" {
   value = module.this.enabled ? coalesce(local.artifactory_output) : null
 }
